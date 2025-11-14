041963ba557bcc8a5c3adbc554a8d4df84cedbce
Nick Michaels <nickmichaels@gmail.com>
2025-11-14 13:09:00 -0700

    Add endpoint for sending a patient referral

--- src/Controller/Api/ProviderController.php
+++ src/Controller/Api/ProviderController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Patient;
 use App\Entity\Provider;
 use App\Entity\Practicioner;
+use App\Entity\PatientReferral;
+use App\Enum\PatientReferralStatus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
@@ -91,23 +94,67 @@ final class ProviderController extends AbstractController
 
     #[Route("/api/providers/{id}/add_practicioner", methods: ["POST"])]
     public function add_practioner(Request $request,
-                                   SerializerInterface $serializer,
                                    EntityManagerInterface $em,
-                                   ValidatorInterface $validator,
                                    Provider $provider): JsonResponse
     {
         $content = $request->getContent();
         $json = json_decode($content);
 
-        $practicionerId = $json->practicioner_id;
-        $practicioner = $em->getRepository(Practicioner::class)->findOneBy(['id' => $practicionerId]);
+        if (!property_exists($json, 'practicioner_id')) {
+            return $this->json("No practicioner id passed in request", 404);
+        }
+        $practicioner = $em->getRepository(Practicioner::class)->findOneBy(
+            ['id' => $json->practicioner_id]
+        );
 
-        if ($practicioner instanceof Practicioner) {
-            $provider->addPracticioner($practicioner);
-            $em->flush();
-            return $this->json("Practicioner added to provider", 200);
-        } else {
+        if (!$practicioner instanceof Practicioner) {
             return $this->json("Practicioner does not exist", 404);
         }
+
+        $provider->addPracticioner($practicioner);
+        $em->flush();
+        return $this->json("Practicioner added to provider", 200);
+    }
+
+    #[Route("/api/providers/{id}/send_referral", methods: ["POST"])]
+    public function send_referral(Request $request,
+                                  EntityManagerInterface $em,
+                                  Provider $provider): JsonResponse
+    {
+        $content = $request->getContent();
+        $json = json_decode($content);
+
+       // die(var_dump($json));
+
+        if (!property_exists($json, 'patient_id')) {
+            return $this->json("No patient id passed in request", 404);
+        } else if (!property_exists($json, 'receiving_provider_id')) {
+            return $this->json("No receiving provider id passed in request", 404);
+        }
+
+        $patient = $em->getRepository(Patient::class)->findOneBy(
+            ['id' => $json->patient_id]
+        );
+        $receiving_provider = $em->getRepository(Provider::class)->findOneBy(
+            ['id' => $json->receiving_provider_id]
+        );
+
+
+
+        if (!$patient instanceof Patient) {
+            return $this->json("Patient does not exist", 404);
+        } elseif (!$receiving_provider instanceof Provider) {
+            return $this->json("Receiving provider does not exist", 404);
+        }
+
+        $patientReferral = new PatientReferral;
+        $patientReferral->setPatient($patient);
+        $patientReferral->setSendingProvider($provider);
+        $patientReferral->setReceivingProvider($receiving_provider);
+        $patientReferral->setStatus(PatientReferralStatus::Pending);
+
+        $em->persist($patientReferral);
+        $em->flush();
+        return $this->json("Patient referral sent", 200);
     }
 }
