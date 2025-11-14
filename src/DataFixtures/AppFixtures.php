b2dddd277e1edcd736fde4497f5e1a19f16d5ab8
Nick Michaels <nickmichaels@gmail.com>
2025-11-14 10:40:46 -0700

    Add more datafixtures for generating dummy data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,29 +2,137 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\User;
+use App\Entity\Patient;
+use App\Entity\Provider;
+use App\Entity\Practicioner;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+
+    private \Faker\Generator $faker;
+
+    private array $specialities;
+
+    public function __construct() {
+        $this->faker = Factory::create();
+
+        $this->specialties = [
+            'Dermatologist',
+            'Anesthesiologist',
+            'Cardiologist',
+            'Oncologist',
+            'Internal medicine',
+            'Immunologist',
+            'Neurologist',
+        ];
+    }
+
+    public function createUsers(ObjectManager $manager)
     {
         // This is just to make an initial ROLE_ADMIN and a ROLE_USER USER 
         $user = new User;
-        $user->setEmail("your_email_here@example.com");
-        $user->setRoles(["ROLE_ADMIN"]);
+        $user->setEmail("nick+3@nick.com");
+        $user->setRoles(["ROLE_USER"]);
         // You can run php bin/console security:hash-password
         // and follow the prompts to hash your own pw
-        $user->setPassword('your_hashed_pw_here');
+        $user->setPassword('$2y$13$LLqUUzwjwDFgRF5FFfNtIeMVmD6WEFiCVcojdKaN4tOYTHKCT1xTC');
         $manager->persist($user);
 
         $user = new User;
-        $user->setEmail("another_email_here@example.com");
+        // $user->setEmail("your_email_here@example.com");
+        $user->setEmail("nick+4@nick.com");
         $user->setRoles(["ROLE_USER"]);
-        $user->setPassword('your_hashed_pw_here');
+        //$user->setPassword('your_hashed_pw_here');
+        $user->setPassword('$2y$13$LLqUUzwjwDFgRF5FFfNtIeMVmD6WEFiCVcojdKaN4tOYTHKCT1xTC');
         $manager->persist($user);
 
         $manager->flush();
+    }
+
+    public function createProviders(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $provider = new Provider;
+            $provider->setName($this->faker->company);
+            $provider->setAddressLine1($this->faker->streetAddress);
+            $provider->setCity($this->faker->city);
+            $provider->setState($this->faker->state);
+            $provider->setZip(intval($this->faker->postcode));
+            $provider->setEmail($this->faker->email);
+            $provider->setPhone($this->faker->phoneNumber);
+            $manager->persist($provider);
+        }
+
+        $manager->flush();
+    }
+
+    public function createPracticioner(ObjectManager $manager)
+    {
+        $jobTitles = [
+            'Doctor', 
+            'Registered Nurse',
+            'Licensed Practical Nurse',
+            'Physical Therapist',
+            'Nursing Assistant',
+            'Physician Assistant',
+            'Radiologic Technologist',
+            'Phlebotomist',
+            'Dietitian',
+        ];
+        for ($i = 0; $i < 10; $i++) {
+            $practicioner = new Practicioner;
+        
+            $practicioner->setName($this->faker->firstname . " "  . $this->faker->lastName);
+            $jobTitle = $jobTitles[array_rand($jobTitles)];
+            $practicioner->setJobTitle($jobTitle);
+            // If we have a doctor, set a specialty
+            if ($jobTitle == 'Doctor') {
+                $practicioner->setSpecialty($this->specialties[array_rand($this->specialties)]);
+            }
+            $practicioner->setLicenseNumber($this->faker->randomNumber);
+            $practicioner->setEmail($this->faker->email);
+            $practicioner->setPhone($this->faker->phoneNumber);
+
+            $manager->persist($practicioner);
+        }
+
+        $manager->flush();
+    }
+
+    public function createPatients(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $patient = new Patient;
+        
+            $patient->setName($this->faker->firstname . " "  . $this->faker->lastName);
+
+            // Defining this as json allows us to add ot this later
+            // and as this is supposed to a small project I dont want 
+            // to get too much in the weeds on details
+            $patientData = [
+                'accountNumber' => $this->faker->randomNumber(8),
+                'DOB' => $this->faker->date,
+                'address' => $this->faker->address,
+            ];
+            $patient->setData($patientData);
+
+            $patient->setEmail($this->faker->email);
+            $patient->setPhone($this->faker->phoneNumber);
+            $manager->persist($patient);
+        }
+
+        $manager->flush();
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //$this->createUsers($manager);
+        $this->createProviders($manager);
+        $this->createPracticioner($manager);
+        $this->createPatients($manager);
     }
 }
